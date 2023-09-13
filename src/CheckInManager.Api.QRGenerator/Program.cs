36520707468f2a5cbd546ef83ba7302755ec3725
Justin Yoo <justin.yoojh@gmail.com>
2023-09-13 19:03:17 +0900

    Update QR code generator API

--- src/CheckInManager.Api.QRGenerator/Program.cs
+++ src/CheckInManager.Api.QRGenerator/Program.cs
@@ -1,12 +1,43 @@
 using CheckInManager.QRGenerator.Services;
 using CheckInManager.QRGenerator.Services.Interfaces;
 
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var host = new HostBuilder()
-    .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(c => c.AddScoped<IQRGeneratorService, QRGeneratorService>())
+    .ConfigureFunctionsWorkerDefaults(worker => worker.UseNewtonsoftJson())
+    .ConfigureHostConfiguration(config => config.AddEnvironmentVariables())
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IOpenApiConfigurationOptions>(_ =>
+        {
+            var options = new OpenApiConfigurationOptions()
+            {
+                OpenApiVersion = OpenApiVersionType.V3,
+                Info = new OpenApiInfo()
+                {
+                    Version = DefaultOpenApiConfigurationOptions.GetOpenApiDocVersion(),
+                    Title = DefaultOpenApiConfigurationOptions.GetOpenApiDocTitle(),
+                    Description = DefaultOpenApiConfigurationOptions.GetOpenApiDocDescription(),
+                    License = new OpenApiLicense()
+                    {
+                        Name = "MIT",
+                        Url = new Uri("http://opensource.org/licenses/MIT"),
+                    }
+                },
+            };
+
+            return options;
+        });
+
+        services.AddScoped<IQRGeneratorService, QRGeneratorService>();
+    })
     .Build();
 
-host.Run();
+await host.RunAsync();
--- src/CheckInManager.Api.QRGenerator/Triggers/GenerateQRCode.cs
+++ src/CheckInManager.Api.QRGenerator/Triggers/GenerateQRCode.cs
@@ -1,45 +1,79 @@
 using System.Net;
 using System.Net.Mime;
 
-using CheckInManager.Core.Models;
 using CheckInManager.QRGenerator.Enums;
 using CheckInManager.QRGenerator.Services.Interfaces;
 
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace CheckInManager.Api.QRGenerator.Triggers;
 
+/// <summary>
+/// This represents the HTTP trigger entity to generate QR code.
+/// </summary>
 public class GenerateQRCode
 {
     private readonly ILogger _logger;
     private readonly IQRGeneratorService _qrGeneratorService;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GenerateQRCode"/> class.
+    /// </summary>
+    /// <param name="loggerFactory"><see cref="ILoggerFactory"/> instance.</param>
+    /// <param name="qrGeneratorService"><see cref="IQRGeneratorService"/> instance.</param>
     public GenerateQRCode(ILoggerFactory loggerFactory, IQRGeneratorService qrGeneratorService)
     {
-        _logger = loggerFactory.CreateLogger<GenerateQRCode>();
-        _qrGeneratorService = qrGeneratorService ?? throw new ArgumentNullException(nameof(qrGeneratorService));
+        this._logger = loggerFactory.ThrowIfNullOrDefault().CreateLogger<GenerateQRCode>();
+        this._qrGeneratorService = qrGeneratorService.ThrowIfNullOrDefault();
     }
 
-    [Function("GenerateQRCode")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    /// <summary>
+    /// Invokes the HTTP trigger endpoint to generate QR code.
+    /// </summary>
+    /// <param name="req"><see cref="HttpRequestData"/> instance.</param>
+    /// <returns>Returns the generated QR code.</returns>
+    [Function(nameof(GenerateQRCode.GenerateAsync))]
+    [OpenApiOperation(operationId: "generate", tags: new[] { "qrcodes" }, Summary = "Generate a QR code from the given input", Description = "This generates a QR code from the given input text.", Visibility = OpenApiVisibilityType.Important)]
+    [OpenApiSecurity(schemeName: "function_key", schemeType: SecuritySchemeType.ApiKey, Name = "x-functions-key", In = OpenApiSecurityLocationType.Header)]
+    [OpenApiRequestBody(contentType: MediaTypeNames.Text.Plain, bodyType: typeof(string), Required = true, Description = "The input for QR code generation.")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: MediaTypeNames.Image.Png, bodyType: typeof(byte[]), Summary = "The generated QR code.", Description = "This returns the QR code generated from the text input.")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.BadRequest, Summary = "Invalid request.", Description = "This indicates the request is invalid.")]
+    [OpenApiResponseWithoutBody(statusCode: HttpStatusCode.InternalServerError, Summary = "Internal server error.", Description = "This indicates the server is not working as expected.")]
+    public async Task<HttpResponseData> GenerateAsync([HttpTrigger(AuthorizationLevel.Function, "POST", Route = "generate")] HttpRequestData req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        this._logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        // todo: 가져온 데이터에서 model 만들 필요.
-        var model = new MeetUpNameTagModel
+        var model = await req.ReadAsStringAsync();
+        if (model is null)
         {
-            Name = "User Name",
-        };
+            this._logger.LogError("Invalid request.");
 
-        var qrData = _qrGeneratorService.Generate(ImageType.Png, model.Name);
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
 
-        _logger.LogInformation($"Is success: {qrData.Length != 0}");
+        try
+        {
+            var qrData = this._qrGeneratorService.Generate(ImageType.Png, model);
 
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", MediaTypeNames.Image.Png);
-        response.WriteBytes(qrData.ToArray());
-        return response;
+            this._logger.LogInformation($"Is success: {qrData.Length != 0}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", MediaTypeNames.Image.Png);
+            response.WriteBytes(qrData);
+
+            return response;
+        }
+        catch (Exception ex)
+        {
+            this._logger.LogError($"Something wrong: {ex.Message}");
+
+            return req.CreateResponse(HttpStatusCode.InternalServerError);
+        }
     }
 }
